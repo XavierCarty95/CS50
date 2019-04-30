ba563970b09e0d71de09d6b329c7244798ca2c75
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-30 19:29:29 +0000

    functions arrays and characters

--- farenheit.c
+++ farenheit.c
@@ -4,18 +4,26 @@
 
 int main()
 {
-    float fahr, celsius;
-    int lower, upper, step;
+    // float fahr, celsius;
+    // int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
 
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
+    // lower = 0;
+    // upper = 300;
+    // step = 20;
+
+    // printf("Converting Celsius into Farenheit\n");
+    // celsius = lower;
+
+    // while (celsius <= upper) {
+    //     fahr = (celsius * 9/5) + 32;
+    //     printf("%6.0f %3.0f\n", celsius , fahr);
+    //     celsius = celsius + step;
+    // }
+
+    int fahr;
+
+    for(fahr = 300; fahr >= 0; fahr = fahr - 20)
+        printf("%3d  %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 
 }
