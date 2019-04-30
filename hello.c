a8db4aa1f347760442d09ba509adde0f3d18c4be
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-30 14:00:47 +0000

    making a farenheit program

--- hello.c
+++ hello.c
@@ -1,25 +1,10 @@
-#include <cs50.h>
-#include <math.h>
 #include <stdio.h>
 
-
-
-int main(void)
+int main()
 {
 
-    // string name = get_string("What is your name? ");
-    // string lname = get_string("What is your last name?");
-    // string title = "Mr/Ms";
-    // printf("Hello %s %s %s\n", title, name,lname);
-
-    int myNum = 36;
-    int myFloat = round(3.87 *100);
-    float other = round(1.52);
-    int total = myFloat + other;
-    int cents = total % 100;
-    int dollars = total /100;
-    printf("adding the numbers: $%i.%i\n", dollars, cents);
-    float division = 78.0/(float)15;
-    printf("the division: %.1f\n", division);
+    printf("Hello, ");
+    printf("World");
+    printf("\n");
 
 }
