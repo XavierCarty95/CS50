2314af2beb62dab41d36416c7b0a3f6454d5aab6
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-05-02 14:38:24 +0000

    added psets folder

---
+++ psets/mario.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(void)
+
+{
+
+    int height , spaces , hashes;
+
+
+   do {
+
+
+        printf("What is the current height?");
+        height = get_int()
+
+   }
+
+
+
+
+}
