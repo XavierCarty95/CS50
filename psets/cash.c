ca2b1506cd2abddee3043d44d15c813a04b8aac9
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-05-04 21:54:25 +0000

    psets

--- psets/cash.c
+++ psets/cash.c
@@ -7,21 +7,19 @@
 int main()
 {
     int dollars;
-     do
-     {
-     float change = get_float("How much change is due : ");
-     cents = round(change * 100);
-     }
-     while(cents < 0);
-     int quarters = cents/25;
-     int dimes = ((cents%25)/10)/5;
-     int nickels =((cents%25)/10);
-     int pennies = ((cents%25)/10 % 5);
-     printf("%d\n", quarters + dimes + nickels + pennies);
+    do
+    {
+        float change = get_float("How much change is due : ");
+        cents = round(change * 100);
+    }
+    while (cents < 0);
+    int quarters = cents / 25;
+    int dimes = ((cents % 25) / 10) / 5;
+    int nickels = ((cents % 25) / 10);
+    int pennies = ((cents % 25) / 10 % 5);
 
-
-     }
-
+    printf("%d\n", quarters + dimes + nickels + pennies);
 
 
 
+}
