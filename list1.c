64227ac4f3509963113360744c225c1d4613820e
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-07-15 18:18:43 -0400

    "list1.c"

---
+++ list1.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int *numbers = NULL;
+    int capacity = 0;
+
+    int size = 0;
+    while(true)
+    {
+        int number = get_int("Number: ");
+
+        if(number == INT_MAX)
+        {
+            break;
+        }
+
+        if(size == capacity){
+            int *tmp = realloc(numbers, sizeof(int) * (size + 1));
+
+        }
+    }
+}
