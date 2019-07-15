c804a2044f929368745d1470aa03856d09203456
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-07-15 18:17:29 -0400

    "struct.c "

---
+++ struct.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "struct.h"
+
+
+
+int main(){
+
+    int enrollment = get_int("Enrollment: ");
+    student students[enrollment];
+
+    for(int i = 0; i < enrollment; i++)
+   {
+       students[i].name = get_string("Name: ");
+       students[i].dorm = get_string("Dorm: ");
+   }
+
+    for(int i = 0; i< enrollment; i++)
+    {
+        printf("%s is in %s. \n", students[i].name , students[i].dorm);
+    }
+}
